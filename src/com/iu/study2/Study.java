0b5357487e6202c4a9dd8ad3399bfaa2cc1393c9
kyungmookchoi <huiae82@naver.com>
2023-06-27 15:53:05 +0900

    start java

---
+++ src/com/iu/study2/Study.java
@@ -0,0 +1,12 @@
+package com.iu.study2;
+
+public class Study {
+
+	public static void main(String[] args) {
+		
+		//Master 브랜치에서 작성함.
+		//test 브랜치에서 작성함.
+		//추가 작업
+	}
+
+}
---
+++ src/com/iu/study2/Study2.java
@@ -0,0 +1,22 @@
+package com.iu.study2;
+
+public class Study2 {
+
+	public static void main(String[] args) {
+		//if, if-else, if-else if-else
+		//switch() case
+		
+		int num=5;
+		
+		switch(num*2) {
+		case 10:
+			System.out.println("num 10");
+			break;
+		}
+	}
+}
+
+
+
+
+
